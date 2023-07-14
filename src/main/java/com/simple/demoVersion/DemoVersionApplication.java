253f6857d1a39f4a080ae66fe49604127e0689c3
Serhii <serhii.mask@gmail.com>
2023-07-14 11:17:33 +0300

    init: first commit

---
+++ src/main/java/com/simple/demoVersion/DemoVersionApplication.java
@@ -0,0 +1,13 @@
+package com.simple.demoVersion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoVersionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoVersionApplication.class, args);
+	}
+
+}
